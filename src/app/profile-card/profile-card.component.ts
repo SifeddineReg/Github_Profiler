8a7b4cf5b05fd09cb11633174085b9974f181e52
sifeddine.regragui <sifeddine.regragui@epita.fr>
2024-07-22 00:46:41 +0200

    feat: github api request added

--- src/app/profile-card/profile-card.component.ts
+++ src/app/profile-card/profile-card.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DatePipe, LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, LowerCasePipe],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.css'
+  styleUrls: ['./profile-card.component.css']
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+  @Input() userProfile: any;
 
+  ngOnInit() {
+    if (!this.userProfile) {
+      this.userProfile = {
+        login: 'Sifeddine',
+        id: 103838543,
+        node_id: 'U_kgDOBjBzTw',
+        avatar_url: 'https://via.placeholder.com/120',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/SifeddineReg',
+        html_url: '',
+        followers_url: '',
+        following_url: '',
+        gists_url: '',
+        starred_url: '',
+        subscriptions_url: '',
+        organizations_url: '',
+        repos_url: '',
+        events_url: '',
+        received_events_url: '',
+        type: '',
+        site_admin: false,
+        name: '',
+        company: '',
+        blog: '',
+        location: '',
+        email: '',
+        hireable: '',
+        bio: 'CS student at EPITA Paris',
+        twitter_username: '',
+        public_repos: 5,
+        public_gists: 0,
+        followers: 3,
+        following: 2,
+        created_at: '2022-04-16T14:28:52Z',
+        updated_at: '2024-07-06T11:18:14Z'
+      };
+      this.userProfile.username = `@${this.userProfile.login}`;
+    }
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  @Output() userProfile = new EventEmitter<any>();
+  constructor() {}
 
+  searchUser() {
+    const username = (document.querySelector('.search_text') as HTMLInputElement).value;
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(data => {
+        this.userProfile.emit(data);
+      });
+  }
 }
