ae05615bfe88d7b7430583851c14b57fe49475bd
sifeddine.regragui <sifeddine.regragui@epita.fr>
2024-07-22 14:31:02 +0200

    feat: fixed not found users and empty bio

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -13,9 +13,50 @@ export class SearchBarComponent {
   searchUser() {
     const username = (document.querySelector('.search_text') as HTMLInputElement).value;
     fetch(`https://api.github.com/users/${username}`)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('User not found');
+        }
+        return response.json();
+      })
       .then(data => {
         this.userProfile.emit(data);
-      });
+      })
+      .catch(error => {
+        this.userProfile.emit({
+          login: 'Jhon Doe',
+          id: 103838543,
+          node_id: 'U_kgDOBjBzTw',
+          avatar_url: 'https://via.placeholder.com/120',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/JhonDoe',
+          html_url: '',
+          followers_url: '',
+          following_url: '',
+          gists_url: '',
+          starred_url: '',
+          subscriptions_url: '',
+          organizations_url: '',
+          repos_url: '',
+          events_url: '',
+          received_events_url: '',
+          type: '',
+          site_admin: false,
+          name: '',
+          company: '',
+          blog: '',
+          location: '',
+          email: '',
+          hireable: '',
+          bio: 'They use me as a placeholder for the examples.',
+          twitter_username: '',
+          public_repos: 5,
+          public_gists: 0,
+          followers: 3,
+          following: 2,
+          created_at: '2022-04-16T14:28:52Z',
+          updated_at: '2024-07-06T11:18:14Z'
+        });
+      })
   }
 }
